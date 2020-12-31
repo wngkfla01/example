2bb52a6d3b24444fae3c5df06fde30d97eadd5d1
wngkfla01 <wngkfla01@gmail.com>
2020-12-31 15:57:30 +0900

    django courser 9 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
 
 
 
